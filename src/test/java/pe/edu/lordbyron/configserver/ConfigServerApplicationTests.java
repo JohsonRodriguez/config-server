140365f3023f16e56364250e9edf9b7ceb520312
Juan Niño <jcns.teknic@gmail.com>
2021-12-04 15:31:05 -0500

    Agregado: configuracion basica de config-server conexion al repositorio privado

---
+++ src/test/java/pe/edu/lordbyron/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.lordbyron.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
